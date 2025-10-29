2d9a80143cad602eb15ed0eddd94706d3e956eb6
Sekujk <alejoseclen@gmail.com>
2025-10-28 21:55:58 -0500

    refactor: correccion de error

--- trading_system/settings.py
+++ trading_system/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'trading_sys',
-        'USER': 'postgress',
+        'USER': 'postgres',
         'PASSWORD': 'admin',
         'HOST': '127.0.0.1',
         'PORT': '5432'
